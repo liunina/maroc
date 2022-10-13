17586e142703795f6026233dd8e4c30edd741ac1
liunina <i19850511@gmail.com>
2022-10-13 13:39:40 +0800

    代码规范

--- Maroc/MCContext.h
+++ Maroc/MCContext.h
@@ -3,63 +3,37 @@
 #import "MCServiceProtocol.h"
 #import "MarocAppDelegate.h"
 #import <Foundation/Foundation.h>
-
 NS_ASSUME_NONNULL_BEGIN
-
-typedef enum {
-    MCEnvironmentDev = 0,
-    MCEnvironmentTest,
-    MCEnvironmentStage,
-    MCEnvironmentProd
-} MCEnvironmentType;
+typedef enum { MCEnvironmentDev = 0, MCEnvironmentTest, MCEnvironmentStage, MCEnvironmentProd } MCEnvironmentType;
 
 @interface MCContext : NSObject <NSCopying>
-
-//global env
+// global env
 @property (nonatomic, assign) MCEnvironmentType env;
-
-//global config
+// global config
 @property (nonatomic, strong, readonly) MCConfig *config;
-
-//application appkey
+// application appkey
 @property (nonatomic, strong) NSString *appkey;
-//customEvent>=1000
+// customEvent>=1000
 @property (nonatomic, assign) NSInteger customEvent;
-
 @property (nonatomic, strong) UIApplication *application;
-
 @property (nonatomic, strong) NSDictionary *launchOptions;
-
 @property (nonatomic, strong) NSString *moduleConfigName;
-
 @property (nonatomic, strong) NSString *serviceConfigName;
-
-//3D-Touch model
+// 3D-Touch model
 @property (nonatomic, strong, readonly) MCShortcutItem *touchShortcutItem;
-
-//OpenURL model
+// OpenURL model
 @property (nonatomic, strong, readonly) MCOpenURLItem *openURLItem;
-
-//Notifications Remote or Local
+// Notifications Remote or Local
 @property (nonatomic, strong, readonly) MCNotificationsItem *notificationsItem;
-
-//user Activity Model
+// user Activity Model
 @property (nonatomic, strong, readonly) MCUserActivityItem *userActivityItem;
-
-//watch Model
+// watch Model
 @property (nonatomic, strong, readonly) MCWatchItem *watchItem;
-
-//custom param
+// custom param
 @property (nonatomic, copy) NSDictionary *customParam;
-
 + (instancetype)shareInstance;
-
 - (void)addServiceWithImplInstance:(id)implInstance serviceName:(NSString *)serviceName;
-
 - (void)removeServiceWithServiceName:(NSString *)serviceName;
-
 - (id)getServiceInstanceFromServiceName:(NSString *)serviceName;
-
 @end
-
 NS_ASSUME_NONNULL_END
--- Maroc/MCContext.m
+++ Maroc/MCContext.m
@@ -1,57 +1,43 @@
 #import "MCContext.h"
 
 @interface MCContext ()
-
 @property (nonatomic, strong) NSMutableDictionary *modulesByName;
-
 @property (nonatomic, strong) NSMutableDictionary *servicesByName;
-
 @property (nonatomic, strong) MCConfig *config;
-
-//3D-Touch model
+// 3D-Touch model
 @property (nonatomic, strong) MCShortcutItem *touchShortcutItem;
-
-//OpenURL model
+// OpenURL model
 @property (nonatomic, strong) MCOpenURLItem *openURLItem;
-
-//Notifications Remote or Local
+// Notifications Remote or Local
 @property (nonatomic, strong) MCNotificationsItem *notificationsItem;
-
-//user Activity Model
+// user Activity Model
 @property (nonatomic, strong) MCUserActivityItem *userActivityItem;
-
-//watch Model
+// watch Model
 @property (nonatomic, strong) MCWatchItem *watchItem;
 @end
 
 @implementation MCContext
-
 + (instancetype)shareInstance {
     static dispatch_once_t p;
     static id MCInstance = nil;
-
     dispatch_once(&p, ^{
-        MCInstance = [[[self class] alloc] init];
-        if ([MCInstance isKindOfClass:[MCContext class]]) {
-            ((MCContext *) MCInstance).config = [MCConfig shareInstance];
-        }
+      MCInstance = [[[self class] alloc] init];
+      if ([MCInstance isKindOfClass:[MCContext class]]) {
+          ((MCContext *)MCInstance).config = [MCConfig shareInstance];
+      }
     });
-
     return MCInstance;
 }
 
 - (void)addServiceWithImplInstance:(id)implInstance serviceName:(NSString *)serviceName {
     [[MCContext shareInstance].servicesByName setObject:implInstance forKey:serviceName];
 }
-
 - (void)removeServiceWithServiceName:(NSString *)serviceName {
     [[MCContext shareInstance].servicesByName removeObjectForKey:serviceName];
 }
-
 - (id)getServiceInstanceFromServiceName:(NSString *)serviceName {
     return [[MCContext shareInstance].servicesByName objectForKey:serviceName];
 }
-
 - (instancetype)init {
     self = [super init];
     if (self) {
@@ -64,13 +50,10 @@
         self.notificationsItem = [MCNotificationsItem new];
         self.userActivityItem = [MCUserActivityItem new];
     }
-
     return self;
 }
-
 - (instancetype)copyWithZone:(NSZone *)zone {
     MCContext *context = [[self.class allocWithZone:zone] init];
-
     context.env = self.env;
     context.config = self.config;
     context.appkey = self.appkey;
@@ -84,8 +67,6 @@
     context.notificationsItem = self.notificationsItem;
     context.userActivityItem = self.userActivityItem;
     context.customParam = self.customParam;
-
     return context;
 }
-
 @end
