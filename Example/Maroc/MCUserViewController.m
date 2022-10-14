3463b15ef3cca0295dc911345ce694ecde98558a
liunina <i19850511@gmail.com>
2022-10-14 10:10:33 +0800

    feat: 测试提交信息格式

--- Example/Maroc/MCUserViewController.m
+++ Example/Maroc/MCUserViewController.m
@@ -19,7 +19,6 @@
     // Do any additional setup after loading the view.
 }
 
-/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -27,6 +26,5 @@
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
-*/
 
 @end
